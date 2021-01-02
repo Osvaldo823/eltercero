6439b38591d6b4d01621a2d7dd00f4dc74e4c173
Oszwaldo_san <osvaldosantos823@gmail.com>
2021-01-01 19:22:00 -0600

    Se menciona a Korra

--- Power.java
+++ Power.java
@@ -3,6 +3,7 @@ public class Power{
   publis static void main(string args[])
   {
     System.out.println("El Avatar Roku no erá tan malo");
+    System.out.println("Korra erá el Avatar más fuerte");
   }
   
 }
